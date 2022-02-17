abce9b14640488fd757813a3a4dd9e057a284358
somenath-00100 <somenathburnwal2@gmail.com>
2022-02-17 15:08:40 +0530

    test file

---
+++ test_prog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fib(int i){
+  if (i==0 ||i==1){
+    return i;
+    }
+  else{
+    return fib(i-1)+fib(i-2);
+  }
+}
+int main(void) {
+  int a;
+  printf("Enter a number:");
+  scanf("%d",&a);
+  for(int i=0;i<a;i++){
+      printf("%d  ",fib(i));
+  }
+    
+}
